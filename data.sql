beebd1225fae2f0317303afcc4a38ccdd063cf08
ibrahim777764 <ibrahimnaseer896@gmail.com>
2022-08-10 23:13:11 +0500

    updated files

--- data.sql
+++ data.sql
@@ -5,4 +5,12 @@ values
 ('Agumon','2020-02-03',0,10.23,true),
  ('Gabumon','2018-11-15',2,8,true), 
  ('Pikachu','2021-01-07',1,15.04,false), 
-('devimon','2017-05-12',5,11,true);
+('devimon','2017-05-12',5,11,true),
+('Charmander','2020-03-08',0,11,false),
+('Plantmon' , '2021-11-15',2,5.7,true),
+('Squirtle','1993-04-02',3,12.13,false),
+('Angemon','2005-06-12',1,45,true),
+('Boarmon','2005-06-07',7,20.4,true),
+('Blossom','1998,10-13',3,17,true),
+('Ditto','2022-05-14',4,22,true);
+
